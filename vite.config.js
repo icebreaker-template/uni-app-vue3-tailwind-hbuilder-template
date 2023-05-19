3812975b20447a80b090d11ae23b6c44a6622c0a
SonOfMagic <qq1324318532@gmail.com>
2023-05-19 13:06:31 +0800

    feat: use weapp-tailwindcss/vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import path from "path";
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
-import { UnifiedViteWeappTailwindcssPlugin as uvwt } from "weapp-tailwindcss-webpack-plugin/vite";
+import { UnifiedViteWeappTailwindcssPlugin as uvwt } from "weapp-tailwindcss/vite";
 // 注意： 打包成 h5 和 app 都不需要开启插件配置
 const isH5 = process.env.UNI_PLATFORM === "h5";
 const isApp = process.env.UNI_PLATFORM === "app";
